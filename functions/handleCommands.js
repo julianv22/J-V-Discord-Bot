3073514b3d2e1f6e44e0fd6892e29af57a0b00de
Julian-V <julianv2212@gmail.com>
2022-07-23 11:29:24 +0700

    11:29 Thứ Bảy, 23 tháng 7, 2022 (GMT+7)

--- functions/handleCommands.js
+++ functions/handleCommands.js
@@ -41,7 +41,7 @@ module.exports = (client) => {
         console.log('\nSuccessfully reloaded application (/) commands.\n');
       })();
     } catch (e) {
-      console.log;
+      console.log(e);
     }
   }
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.send('Hello Julian-V! Have a good day 🥰')
 });
 
 app.listen(3000, () => {
